f1b1511e7af2ddcd043aa0d87df58500e7905a2b
khachoang <hoangnguyen@KHACHOANG>
2017-06-15 11:34:23 +0300

    20170615

--- Test_rpm2600_load200/fitting.m
+++
@@ -1,9 +0,0 @@
-function [b] = fitting(x1,x2,x3,y)
-xdata = generateData(x1,x2,x3);
-f = @(b,xdata)(b(1) + b(2)*xdata(:,2) + b(3)*xdata(:,3) + b(4)*xdata(:,4)...    %Linear terms
-        + b(5)*xdata(:,5) + b(6)*xdata(:,6) + b(7)*xdata(:,7)...                  %Interaction terms
-        + b(8)*xdata(:,8) + b(9)*xdata(:,9) + b(10)*xdata(:,10));                  %Quadratic terms
-x0 = zeros(1,10);
-OPTIONS = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
-b = lsqcurvefit(f,x0,xdata,y,[],[],OPTIONS);
-end
--- Test_rpm2600_load200/generateData.m
+++
@@ -1,4 +0,0 @@
-function xdata = generateData(x1,x2,x3)
-    xdata = [ones(length(x1),1) x1 x2 x3 x1.*x2 x1.*x3 x2.*x3 x1.^2 x2.^2 x3.^2];
-    %xdata = [x1 x2 x3 x1.*x2 x1.*x3 x2.*x3 x1.^2 x2.^2 x3.^2];
-end
